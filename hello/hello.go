9629edab8a13a5616d19fe6b6ef8723cd6058afc
Robert Gaal <robert@gaal.co>
2017-01-25 14:18:16 +0100

    Remade into TQ Slack bot

--- hello/hello.go
+++ hello/hello.go
@@ -10,5 +10,14 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+  //Read the Request Parameter "command"
+  command := r.FormValue("command")
+
+  // TODO: Other checks for tokens/username/etc
+
+  if command == "/menu" {
+   fmt.Fprint(w,"See the full club menu at http://go.tq.co/menu. Nom!")
+  } else {
+    fmt.Fprint(w,"Error")
+  }
 }
